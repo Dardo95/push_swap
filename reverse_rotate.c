fc29f88125e6294a27282234d7c1725177c35f05
Eduardo Nogueira Santo-Tomas <enogueir@c2r18s6.42madrid.com>
2024-12-03 16:51:38 +0100

    act

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -1,44 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rotate.c                                           :+:      :+:    :+:   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enogueir <enogueir@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/03 13:53:22 by enogueir          #+#    #+#             */
-/*   Updated: 2024/12/03 14:42:34 by enogueir         ###   ########.fr       */
+/*   Created: 2024/12/03 14:47:05 by enogueir          #+#    #+#             */
+/*   Updated: 2024/12/03 16:51:02 by enogueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rotate(t_stack *stack, char c, int s_n_bool)
+void	reverse_rotate(t_stack *stack, char c, int s_n_bool)
 {
 	t_node	*temp;
-	t_node	*last;
+	t_node	*last:
 
 	if (stack->head == NULL || stack->size <= 1)
 	{
 		return ;
 	}
-	temp = stack->head;
-	stack->head = stack->head->next;
-	last = stack->head;
-	while (last->next != NULL)
-		last = last->next;
-	last->next = temp;
-	temp = NULL;
-	if (s_n_bool)
-	{
-		write(1, "r", 1);
-		write(1, &c, 1);
-		write(1, "\n", 1);
-	}
-}
 
-void	rotate_both(t_stack *stack_a, t_stack *stack_b)
-{
-	rotate(stack_a, 'a', 0);
-	rotate(stack_b, 'b', 0);
-	ft_putstr_fd("rr\n", 1);
+	last = stack->head;
 }

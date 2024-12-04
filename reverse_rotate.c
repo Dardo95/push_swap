8b9660914557050a411420de4e50bc04f9a2fa32
Dardo95 <dsanto-tomas@hotmail.com>
2024-12-04 13:06:47 +0100

    act

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -14,13 +14,32 @@
 
 void	reverse_rotate(t_stack *stack, char c, int s_n_bool)
 {
-	t_node	*temp;
-	t_node	*last:
+	t_node	*last;
+	t_node	*second_last;
 
 	if (stack->head == NULL || stack->size <= 1)
-	{
 		return ;
-	}
-
 	last = stack->head;
+	second_last = NULL;
+	while (last->next != NULL)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->next = stack->head;
+	stack->head = last;
+	if (s_n_bool)
+	{
+		write(1, "rr", 2);
+		write(1, &c, 1);
+		write(1, "\n", 1);
+	}
+}
+
+void	reverse_rotate_both(t_stack *stack_a, t_stack *stack_b)
+{
+	reverse_rotate(stack_a, 'a', 0);
+	reverse_rotate(stack_b, 'b', 0);
+	ft_putstr_fd("rrr\n", 1);
 }

bf796175bd6e34fb41f2cea267025db75edcef76
Dardo95 <dsanto-tomas@hotmail.com>
2024-12-09 18:26:45 +0100

    2024-12-09 18:26:45

--- parse.c
+++ parse.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enogueir <enogueir@student.42madrid>       +#+  +:+       +#+        */
+/*   By: enogueir <enogueir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 16:46:38 by enogueir          #+#    #+#             */
-/*   Updated: 2024/12/04 17:41:03 by enogueir         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:45:28 by enogueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+size_t  count_numbers(int argc, char  **argv)
+{
+    int i;
+    int j;
+    int result;
+
+    i = 0;
+    result = 0;
+    while (i < argc - 1)
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            if (!ft_isdigit(argv[i][j]) && argv[i][j] != ' '
+                && argv[i][j] != '+' && argv[i][j] != '-')
+            {
+                
+            }
+        }
+        i++;
+    }
+}
 
 int	split_arg(int argc, char **argv)
 {

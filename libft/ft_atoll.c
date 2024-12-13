9d485369edaf63e2a7af4f56ab224e2324ad050b
Dardo95 <dsanto-tomas@hotmail.com>
2024-12-13 12:15:59 +0100

    ft_atoll add 2024-12-13 12:15:59

---
+++ libft/ft_atoll.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryner <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 11:40:24 by ryner             #+#    #+#             */
+/*   Updated: 2024/12/13 11:40:32 by ryner            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h";
+
+long long atoll(const char *nptr)
+{
+	long long sign;
+	long long result;
+	size_t i;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-' )
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
